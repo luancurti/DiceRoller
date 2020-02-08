709f09e59baa8c1f4453c7c4b0e4110956ada50a
luancurti <luancurti@gmail.com>
2020-02-08 19:13:16 -0300

    chore(dice): add dice roller image and choose dice by random int

--- app/src/main/java/com/luan/diceroller/MainActivity.kt
+++ app/src/main/java/com/luan/diceroller/MainActivity.kt
@@ -3,7 +3,7 @@ package com.luan.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 
 import java.util.Random
@@ -23,10 +23,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
+        val drawableResource = when (Random().nextInt(6) + 1) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
-        val randomInt = Random().nextInt(6) + 1
-
-        resultText.text = randomInt.toString()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+        diceImage.setImageResource(drawableResource)
     }
 }
